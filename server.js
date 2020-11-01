d6213c12e3b81222ed1c456a6e10b95060031f86
Allegra Anka <allegra.anka@piano.io>
2020-11-01 18:15:54 -0500

    first part of oauth flow working properly, still need to get the redirect working after token endpoint is hit and get calendar data for the user displayed

--- server.js
+++ server.js
@@ -11,12 +11,11 @@ const app = express();
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('Hello Chronologer!');
-});
+app.set('view engine', 'ejs');
+app.set('views', __dirname);
 
-app.get('/login', (req, res) => {
-  const oauth2Client = new OAuth2(
+app.get('/', (req, res) => {
+  const oauth2Client = new OAuth2 (
     CONFIG.oauth2Credentials.client_id, 
     CONFIG.oauth2Credentials.client_secret, 
     CONFIG.oauth2Credentials.redirect_uris[0]
@@ -27,12 +26,29 @@ app.get('/login', (req, res) => {
     scope: CONFIG.oauth2Credentials.scopes // Using the access scopes from our config file
   });
 
-  return res.send({ loginLink: loginLink });
+  return res.render("index", { loginLink: loginLink });
 });
 
 app.get('/authorized', (req, res) => {
-    res.send('Hello Authorized Chronologer. This is where we will display your data.');
-  });
+  const oauth2Client = new OAuth2 (
+    CONFIG.oauth2Credentials.client_id, 
+    CONFIG.oauth2Credentials.client_secret, 
+    CONFIG.oauth2Credentials.redirect_uris[0]
+  );
+
+  if (req.query.error) {
+    res.redirect('/');
+  } else {
+    oauth2Client.getToken(req.query.code, (error, token) => {
+      if (error) {
+        res.redirect('/');
+      }
+
+      res.cookie('jwt', jwt.sign(token, CONFIG.JWTsecret));
+      return res.redirect('/google-calendar-data');
+    });
+  }
+});
 
 app.listen(CONFIG.port, () => {
   console.log(`Chronolog tool listening on port ${CONFIG.port}!`);
